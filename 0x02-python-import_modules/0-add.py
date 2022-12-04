7b5f8b73d0f8f7e515a1cc94e5e4128d46422077
Yoliyolanda <ymugabe25@gmail.com>
2022-12-04 09:50:25 -0800

    tasks

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,8 @@
 
 """Print the sum of 1 and 2."""
 f __name__ == "__main__":
-        from add_0 import add
+    from add_0 import add
 
-        a = 1
-        b = 2
-        print('{:d} + {i:d} = {:d}'.format(a, b, add(a, b)))
+a = 1
+b = 2
+print('{:d} + {i:d} = {:d}'.format(a, b, add(a, b)))
