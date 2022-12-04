20f0d64fb64e8dae7058a6cc22f7e26f15fc9cac
Yoliyolanda <ymugabe25@gmail.com>
2022-12-04 08:52:29 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
+"""Print the sum of 1 and 2."""
 f __name__ == "__main__":
         from add_0 import add
+
         a = 1
         b = 2
         print('{:d} + {i:d} = {:d}'.format(a, b, add(a, b)))
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def add(a, b):
+    """My addition fuction
+
+    Args:
+        a: first integer
+        b: second integer
+
+        Returns:
+            The return value. a + b"""
     return(a+b)
