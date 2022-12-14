e1d25edd575b99943f1055ff83d415990e973a1b
Yoliyolanda <ymugabe25@gmail.com>
2022-12-13 18:52:36 -0800

    tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,9 +13,9 @@ if (!list)
 return (0);
 while (slow && fast && fast->next)
 {
-slow =slow->next 
+slow = slow->next
 fast = fast->next->next;
-if (slow == fast)
+if (slow == fast
 return (1);
 }
 return (0);
