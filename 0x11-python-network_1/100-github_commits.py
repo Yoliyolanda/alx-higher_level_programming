736f3c5c1dcae25ae2b2e9c4a60b3e47e285374c
Yoliyolanda <ymugabe25@gmail.com>
2023-03-31 12:33:13 -0700

    tasks

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""This Python script takes 2 arguments in order to solve the  challenge that list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails” 
+"""This Python st 10 commits (from the most recent to oldest) 
 """
 import sys
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
